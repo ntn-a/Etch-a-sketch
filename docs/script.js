bd90939ea062665d5a5928364b458feaf9e21c83
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-05-01 16:09:22 -0700

    Added colors, basic functionality.

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,18 @@
-let loadGrid = () =>{
+let loadGrid = (num) =>{
     let container = document.querySelector('.grid');
-    for (let i = 0; i < 16*16; i++){
+    for (let i = 0; i < num*(num+1); i++){
         let box = document.createElement('div');
-        box.classList.add('box');
+        box.classList.add('blank');
         container.appendChild(box);
     }
 }
 
-loadGrid();
+loadGrid(16);
+
+let grid = document.querySelectorAll('.blank');
+
+grid.forEach((blank) => {
+    blank.addEventListener('mouseover', () => {
+        blank.classList.add('active');
+    });
+});
